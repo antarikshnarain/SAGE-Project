e173e0269e1e89f269ad6881f47e2b2318da13a1
Antariksh <antariksh.cloud@gmail.com>
2020-01-09 15:10:33 -0800

    Updated code to print for all dates

--- webpage_reader.py
+++ webpage_reader.py
@@ -1,7 +1,7 @@
 import requests as rq
 import re
 from bs4 import BeautifulSoup as bs
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.parser import parse
 import pytz
 """
@@ -24,7 +24,22 @@ class PageReader:
         soup = bs(response.text,'html5lib')
         tables = soup.find_all('table', class_='wikitable')
         # Send Table 1 for Orbital Satellite data
-        self.extract_data(tables[0])
+        records = self.extract_data(tables[0])
+
+        # Create records date wise
+        #date = pytz.utc.localize(date)
+        present_date = pytz.utc.localize(parse("2019-01-01"))
+        count = 0
+        for rec in records:
+            while present_date < rec[0]:
+                print ("%s,%d"%(present_date.isoformat(),count))
+                present_date = present_date + timedelta(days=1)
+                count = 0
+            if present_date == rec[0]:
+                count += 1
+            else:
+                print("%s,%d", present_date, count)
+                count = 0
 
     
     def extract_data(self, table_data):
@@ -32,25 +47,38 @@ class PageReader:
         Function responsible for extracting data from the website
         """
         table_rows = table_data.find_all('tr')
+        was_success = False
+        rec_date = None
+        records = []
         for i in range(4,len(table_rows)):
             row_data = table_rows[i].find_all('td')
             if len(row_data) == 5:
+                # Add record if successful
+                if rec_date != None and was_success:
+                    records.append((rec_date, was_success))
                 # Extract Date
                 rec_date = self.clean_data_date(row_data[0].getText())
-                print("---------------------------------")
-                print(rec_date, end='\t')
+                was_success = False
+                #print("---------------------------------")
+                #print(rec_date, end='\t')
             elif len(row_data) == 4:
                 # Previous date to be used
                 pass
             elif len(row_data) == 6:
                 # Satellite info for rec_date
-                rec_flight_status = self.clean_data(row_data[5].text)
-                print(rec_flight_status, end='\t')
+                rec_flight_status = self.clean_data(row_data[5].text).lower()
+                if rec_flight_status in ["operational", "en route", "successful"]:
+                    # Can be changed for count
+                    was_success = True
+                #print(rec_flight_status, end='\t')
+        
+        return records
     
     def clean_data(self, text):
         expressions = [
             r"<br>",
-            r"<sub+"
+            r"<sub+",
+            r"[^ \w]"
         ]
         for expression in expressions:
             text = re.sub(expression, "", text)
@@ -61,6 +89,7 @@ class PageReader:
         date = re.sub(r"[^ \w\d:].*","",date)
         #date = datetime.strptime(date,'%d %B%H:%M')
         date = parse(date)
-        date = date.replace(year=2019)
+        # Loosing Time data
+        date = date.replace(year=2019,hour=0,minute=0,second=0,microsecond=0)
         date = pytz.utc.localize(date)
-        return date.isoformat()
+        return date
